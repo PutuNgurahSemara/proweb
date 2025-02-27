642b2d599bc8742340b7f471c5235aa4b40e77fe
PutuNgurahSemara <ngurahgondol1260@gmail.com>
2025-02-27 15:27:23 +0800

    tugas 3

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,32 @@ app.post('/data', (req, res) => {
 res.send(`Data diterima: ${nama}`);
 });
 
+// CREATE
+app.post('/produk', async (req, res) => {
+    const { nama, harga } = req.body;
+    try {
+      const newProduk = await pool.query(
+        'INSERT INTO produk (nama, harga) VALUES ($1, $2) RETURNING *',
+        [nama, harga]
+      );
+      res.json(newProduk.rows[0]);
+    } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+    }
+   });
+
+    // READ
+ app.get('/produk', async (req, res) => {
+    try {
+    const allProduk = await pool.query('SELECT * FROM produk');
+    res.json(allProduk.rows);
+    } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+    }
+    });
+
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
 });
